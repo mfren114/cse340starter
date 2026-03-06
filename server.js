0b8dcf64b1c97f72a1187b5a76895a15e4d737d8
mfren114 <mathew.frenette@yahoo.com>
2026-03-06 13:14:12 -0800

    Update

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ app.set("layout", "./layouts/layout")
  * Routes
  *************************/
 app.use(static)
+//Index route
+app.get("/", function(req,res) {
+  res.render("index", {title: "Home"})
+}) 
 
 /* ***********************
  * Local Server Information
