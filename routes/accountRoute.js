7ba9fadfc3aae2c487fd8a5c0ac177c2a5d00db6
mfren114 <mathew.frenette@yahoo.com>
2026-03-25 10:20:34 -0700

    form added

---
+++ routes/accountRoute.js
@@ -0,0 +1,9 @@
+//Needed Resources
+const express = require("express")
+const router = new express.Router()
+const utilities = require("../utilities/")
+const accountController = require("../controllers/accountController")
+
+router.get("/login", (accountController.buildLogin))
+
+module.exports = router;
--- server.js
+++ server.js
@@ -13,6 +13,30 @@ const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
 const utilities = require("./utilities/")
+const session = require("express-session")
+const pool = require("./database")
+const accountRoute = require("./routes/accountRoute")
+
+/* ***********************
+ * Middleware
+ * ************************/
+ app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
 
 /* ***********************
  * View Engine and Templates
@@ -29,6 +53,9 @@ app.get("/", baseController.buildHome)
 
 // Inventory routes
 app.use("/inv", inventoryRoute)
+
+// Account Route
+app.use("/account", accountRoute)
 
 /* ***********************
  * Local Server Information
