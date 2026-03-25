ddb66a1b8de9516cd507c1def4acec2b7b668442
mfren114 <mathew.frenette@yahoo.com>
2026-03-25 14:38:27 -0700

    registry data saved

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -6,4 +6,8 @@ const accountController = require("../controllers/accountController")
 
 router.get("/login", (accountController.buildLogin))
 
+router.get("/register", (accountController.buildRegister))
+
+router.post('/register', (accountController.registerAccount))
+
 module.exports = router;
